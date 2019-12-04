78949fae829bc56d6116adee5aca8032e80f9287
haileykoo <haileykwon8@gmail.com>
2019-12-04 18:51:34 -0500

    item added

--- js/index.js
+++ js/index.js
@@ -32,5 +32,192 @@ const items = [
         scents: `Spicy`,
         size: 90,
         brand: `YVES SAINT LAURENT`
+    }, { //3
+        id: 104,
+        name: `REPLICA’ By The Fireplace`,
+        image: `https://www.sephora.com/productimages/sku/s1788058-main-zoom.jpg`,
+        description: `These notes combine to make this Maison Margiela perfume the ultimate unisex cologne.`,
+        price: `$159.00`,
+        quantity: 10,
+        category: `Eau de Parfum Spray`,
+        scents: `Warm`,
+        size: 100,
+        brand: `MAISON MARGIELA`
+    }, { //4
+        id: 105,
+        name: `Not A Perfume`,
+        image: `https://www.sephora.com/productimages/sku/s1703321-main-zoom.jpg`,
+        description: `This minimalist, fresh, and clean fragrance is hypoallergenic and ideal for those who don’t normally wear perfume.`,
+        price: `$154.00`,
+        quantity: 50,
+        category: `Eau de Parfum Spray`,
+        scents: `Woody`,
+        size: 100,
+        brand: `JULIETTE HAS A GUN`
+    }, { //5
+        id: 106,
+        name: `Bright Crystal`,
+        image: `https://www.sephora.com/productimages/sku/s1063619-main-zoom.jpg`,
+        description: `Bright Crystal is a fresh, sensual blend of refreshing chilled yuzu and pomegranate mingled with soothing blossoms of peony, magnolia, and lotus flower, warmed with notes of musk and amber.`,
+        price: `$126.00`,
+        quantity: 40,
+        category: `Eau de Toilette Spray`,
+        scents: `Floral`,
+        size: 90,
+        brand: `VERSACE`
+    }, { //6
+        id: 107,
+        name: `Daisy`,
+        image: `https://www.sephora.com/productimages/sku/s1029941-main-zoom.jpg`,
+        description: `Charmingly simple with a signature quality, Daisy Marc Jacobs transports you to a place that’s optimistic, beautiful, and pure.`,
+        price: `$98.00`,
+        quantity: 20,
+        category: `Eau de Toilette Spray`,
+        scents: `Floral`,
+        size: 50,
+        brand: `MARC JACOBS FRAGRANCES`
+    }, { //8
+        id: 109,
+        name: `REPLICA’ By The Fireplace Travel Spray`,
+        image: `https://www.sephora.com/productimages/sku/s2047629-main-zoom.jpg`,
+        description: `By the Fireplace is a warm and spicy fragrance that evokes the comforting sensation of a crackling fireplace beside a frosted winter landscape with enveloping notes of clove, chestnut, and vanilla accord.`,
+        price: `$38.00`,
+        quantity: 33,
+        category: `Eau de Toilette Travel Spray`,
+        scents: `Warm`,
+        size: 10,
+        brand: `MAISON MARGIELA`
+    }, { //9
+        id: 110,
+        name: `REPLICA’ By The Fireplace Travel Spray`,
+        image: `https://www.sephora.com/productimages/sku/s1838234-main-zoom.jpg`,
+        description: `Dressed in a dazzling shade of emerald green, the crystalline flacon of Divine Decadence celebrates a new facet of Marc Jacobs' iconic handbag, the ultimate luxury accessory.`,
+        price: `$120.00`,
+        quantity: 30,
+        category: `Eau de Parfum Spray`,
+        scents: `Earthy`,
+        size: 50,
+        brand: `MARC JACOBS FRAGRANCES`
+    }, { //10
+        id: 111,
+        name: `Amazing Grace Eau de Toilette`,
+        image: `https://www.sephora.com/productimages/sku/s607689-main-zoom.jpg`,
+        description: `Embrace your beauty and express your femininity with Amazing Grace Eau de Toilette.`,
+        price: `$65.00`,
+        quantity: 100,
+        category: `Eau de Toilette Spray`,
+        scents: `Floral`,
+        size: 60,
+        brand: `PHILOSOPHY`
+    }, { //11
+        id: 112,
+        name: `Flowerbomb Nectar`,
+        image: `https://www.sephora.com/productimages/sku/s2042984-main-zoom.jpg`,
+        description: `A sensual blend of bewitching flowers, flowerbomb nectar completes the flowerbomb collection. `,
+        price: `$156.00`,
+        quantity: 20,
+        category: `Eau de Parfum Spray`,
+        scents: `Warm`,
+        size: 50,
+        brand: `VIKTOR&ROLF`
+    }, { //12
+        id: 113,
+        name: `REPLICA Under the Lemon Trees Eau de Toilette`,
+        image: `https://www.sephora.com/productimages/sku/s2177996-main-zoom.jpg`,
+        description: `This fragrance for women and men is a dreamy blend of kalamansi lime, green tea, citrus and soft musk perfumes.`,
+        price: `$159.00`,
+        quantity: 70,
+        category: `Eau de Parfum Spray`,
+        scents: `Fresh`,
+        size: 100,
+        brand: `MAISON MARGIELA`
+    }, { //13
+        id: 114,
+        name: `Amazing Grace Ballet Rose Eau de Toilette`,
+        image: `https://www.sephora.com/productimages/sku/s2039006-main-zoom.jpg`,
+        description: `Introducing Amazing Grace Ballet Rose, which is inspired by the delicate scent of pink rose petals opening at first bloom and the beautiful femininity of philosophy’s bestselling Amazing Grace fragrance.`,
+        price: `$65.00`,
+        quantity: 7,
+        category: `Eau de Toilette Spray`,
+        scents: `Floral`,
+        size: 60,
+        brand: `PHILOSOPHY`
+    }, { //14
+        id: 115,
+        name: `Blackberry & Bay Cologne`,
+        image: `https://www.sephora.com/productimages/sku/s1946706-main-zoom.jpg`,
+        description: `This fragrance evokes childhood memories of blackberry picking—a burst of deep, tart blackberry juice blends with the freshness of just-gathered bay and brambly woods for a vibrant and verdant impression.`,
+        price: `$184.00`,
+        quantity: 20,
+        category: `Spray`,
+        scents: `Fresh`,
+        size: 100,
+        brand: `JO MALONE LONDON`
+    }, { //15
+        id: 116,
+        name: `Red Roses Cologne`,
+        image: `https://www.sephora.com/productimages/sku/s2006021-main-zoom.jpg`,
+        description: `Inspired by a voluptuous blend of seven of the world's most exquisite roses, this fragrance captures the essence of modern romance. `,
+        price: `$184.00`,
+        quantity: 90,
+        category: `Spray`,
+        scents: `Floral`,
+        size: 100,
+        brand: `JO MALONE LONDON`
+    }, { //16
+        id: 117,
+        name: `Oud Wood Travel Spray`,
+        image: `https://www.sephora.com/productimages/sku/s2101350-main-zoom.jpg`,
+        description: `Oud wood is one of the most rare, precious, and expensive ingredients in a perfumer’s arsenal.`,
+        price: `$75.00`,
+        quantity: 9,
+        category: `Eau de Toilette Travel Spray`,
+        scents: `Earthy`,
+        size: 10,
+        brand: `TOM FORD`
+    }, { //17
+        id: 118,
+        name: `Alien Travel Spray`,
+        image: `https://www.sephora.com/productimages/sku/s2103190-main-zoom.jpg`,
+        description: `Alien is a floral-woody-amber fragrance that borders the realms of spirituality and sensuality. It exudes a positive energy, charged with sensual and intriguing mystery.`,
+        price: `$39.00`,
+        quantity: 90,
+        category: `Eau de Toilette Travel Spray`,
+        scents: `Floral`,
+        size: 10,
+        brand: `MUGLER`
+    }, { //18
+        id: 119,
+        name: `Lost Cherry`,
+        image: `https://www.sephora.com/productimages/sku/s2251148-main-zoom.jpg`,
+        description: `Black Cherry’s ripe flesh dripping in cherry liqueur glistens with a teasing touch of bitter almond.`,
+        price: `$535.00`,
+        quantity: 2,
+        category: `Eau de Parfum Spray`,
+        scents: `Warm`,
+        size: 100,
+        brand: `TOM FORD`
+    }, { //19
+        id: 120,
+        name: `Vert d’Encens`,
+        image: `https://www.sephora.com/productimages/sku/s1855527-main-zoom.jpg`,
+        description: `The unbridled nature of the Corsican Coast, known as "the perfumed isle", inspires this distinguished scent’s sophisticated, aromatic, and wild side.`,
+        price: `$725.00`,
+        quantity: 1,
+        category: `Eau de Parfum Decanter`,
+        scents: `Spicy`,
+        size: 250,
+        brand: `TOM FORD`
+    }, { //20
+        id: 121,
+        name: `Donna Born In Roma Eau de Parfum`,
+        image: `https://www.sephora.com/productimages/sku/s2249712-main-zoom.jpg`,
+        description: `The couture elegance is made of three qualities of jasmine which bring a luxurious femininity when blended with vanilla bourbon, the most expensive extract in the world.`,
+        price: `$39.00`,
+        quantity: 15,
+        category: `Eau de Parfum Spray`,
+        scents: `Floral`,
+        size: 10,
+        brand: `VALENTINO`
     }
 ]
