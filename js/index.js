2b5b82d956cd2ab8c29bc657b60a2f628951f22e
haileykoo <haileykwon8@gmail.com>
2019-12-04 16:25:12 -0500

    3 item added

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const items = [
+    { //0
+        id: 101,
+        name: `Saint Eau de Parfum`,
+        image: `https://www.sephora.com/productimages/sku/s1957216-main-zoom.jpg`,
+        description: `Meet Saint—soft, alluring, and dreamy, this gourmand fragrance sweetens a bouquet of jasmine and tiaré flower with notes of mandarin, caramel, and sparkling Mirabelle plum.`,
+        price: `$69.00`,
+        quantity: 50,
+        category: `Eau de Parfum Spray`,
+        scents: `Floral`,
+        size: 100,
+        brand: `KAT VON D`
+    }, { //1
+        id: 102,
+        name: `Flowerbomb`,
+        image: `https://www.sephora.com/productimages/sku/s1377159-main-zoom.jpg`,
+        description: `Deep amber, woody, and patchouli notes combine with warm vanilla and praline notes to complete a truly addictive fragrance.`,
+        price: `$199.00`,
+        quantity: 30,
+        category: `Eau de Parfum Spray`,
+        scents: `Spicy`,
+        size: 100,
+        brand: `VIKTOR&ROLF`
+    }, { //2
+        id: 103,
+        name: `Black Opium`,
+        image: `https://www.sephora.com/productimages/sku/s1688852-main-zoom.jpg`,
+        description: `Black Opium is the highly addictive feminine fragrance from Yves Saint Laurent.`,
+        price: `$162.00`,
+        quantity: 30,
+        category: `Eau de Parfum Spray`,
+        scents: `Spicy`,
+        size: 90,
+        brand: `YVES SAINT LAURENT`
+    }
+]
